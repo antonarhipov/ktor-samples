11f87d9f7265ba51a493e442e56605d0c88c4e96
Anton Arhipov <ant.arhipov@gmail.com>
2021-03-15 12:05:36 +0200

    1) Gradle version update 2) build.gradle -> build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+val prometeus_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven { url = uri("https://kotlin.bintray.com/ktor") }
+}
+
+sourceSets {
+    main {
+        java.srcDir("src")
+        resources {
+            srcDir("resources")
+        }
+    }
+    test {
+        java.srcDir("test")
+    }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-html-builder:$ktor_version")
+    implementation("io.ktor:ktor-locations:$ktor_version")
+    implementation("io.ktor:ktor-metrics-micrometer:$ktor_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-server-host-common:$ktor_version")
+    implementation("io.ktor:ktor-websockets:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.micrometer:micrometer-registry-prometheus:$prometeus_version")
+    implementation("com.google.code.gson:gson:2.8.1")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+
+    //region webjars
+    implementation("io.ktor:ktor-webjars:$ktor_version")
+    implementation("org.webjars:vue:2.1.3")
+    implementation("org.webjars:ionicons:2.0.1")
+    implementation("org.webjars.npm:google-polyline:1.0.0")
+    //endregion
+
+}
